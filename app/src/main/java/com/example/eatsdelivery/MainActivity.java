e7fe70cf7d330fb19bb25f88fef7c22c1a9aa494
Steven <svengra01@estudiantec.cr>
2021-09-03 00:14:07 -0600

    Pantalla Login

--- app/src/main/java/com/example/eatsdelivery/MainActivity.java
+++ app/src/main/java/com/example/eatsdelivery/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.eatsdelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+    }
+
+    public void imprimir(View view){
+
+        System.out.println(username.getText() + " " + password.getText());
     }
 }
