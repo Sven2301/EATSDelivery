a01de5047da20dc0ea730fbdb05c492374c3a64d
Steven <svengra01@estudiantec.cr>
2021-09-06 22:48:35 -0600

    RestaurantMenu

--- app/src/main/java/com/example/eatsdelivery/MainMenu.java
+++ app/src/main/java/com/example/eatsdelivery/MainMenu.java
@@ -2,7 +2,9 @@ package com.example.eatsdelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void seeRestaurantList(View view){
+
+        Intent next = new Intent(this, RestaurantMenu.class);
+        startActivity(next);
     }
 }
