632d2697b1dcfec98439101feb7d64a4d404f28b
Steven <svengra01@estudiantec.cr>
2021-09-08 21:48:53 -0600

    OrderWaitScreen

--- app/src/main/java/com/example/eatsdelivery/OrderWaitScreen.java
+++ app/src/main/java/com/example/eatsdelivery/OrderWaitScreen.java
@@ -6,23 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Cart extends AppCompatActivity {
+public class OrderWaitScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_order_wait_screen);
     }
 
     public void back(View view){
 
         Intent next = new Intent(this, RestaurantMenu.class);
-        startActivity(next);
-    }
-
-    public void confirmOrder(View view){
-
-        Intent next = new Intent(this, OrderWaitScreen.class);
         startActivity(next);
     }
 }
