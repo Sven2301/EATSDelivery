76fe488d593b92857bec36f98bdabd4f1a195c64
Steven <svengra01@estudiantec.cr>
2021-09-15 18:35:39 -0600

    Some SQL

--- app/src/main/java/com/example/eatsdelivery/SQLite/ConexionSQLite.java
+++ app/src/main/java/com/example/eatsdelivery/SQLite/ConexionSQLite.java
@@ -12,15 +12,20 @@ public class ConexionSQLite extends SQLiteOpenHelper {
     final String SQL_CREATE_TipoAcceso = "CREATE TABLE TipoDeAcceso (id INTEGER PRIMARY KEY AUTOINCREMENT, Descripcion TEXT, Tipo INTEGER)";
     final String SQL_CREATE_Usuario    = "CREATE TABLE Usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, TipoAccesoID INTEGER, Usuario TEXT, Nombre TEXT," +
             "FOREIGN KEY(TipoAccesoID) REFERENCES TipoDeAcceso(id))";
-    /*
     final String SQL_CREATE_TipoComida = "CREATE TABLE TipoDeComida (id INTEGER PRIMARY KEY AUTOINCREMENT, Descripcion TEXT)";
     final String SQL_CREATE_Tarjeta = "CREATE TABLE Tarjeta (id INTEGER PRIMARY KEY AUTOINCREMENT, NombrePropiertario TEXT, " +
             "NumeroTarjeta INTEGER, CCV INTEGER, FechaVencimiento TEXT)";
     final String SQL_CREATE_Plato = "CREATE TABLE Plato (id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Costo INTEGER, Descripcion TEXT)";
+    final String SQL_CREATE_Direccion = "CREATE TABLE Direccion (id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Descripcion TEXT, DireccionExacta TEXT)";
+    final String SQL_CREATE_Restaurante = "CREATE TABLE Restaurante (id INTEGER PRIMARY KEY AUTOINCREMENT, DireccionID INTEGER," +
+            " TipoComidaID INTEGER, Nombre TEXT, Activo INTEGER, FOREIGN KEY(DireccionID) REFERENCES Direccion(id)," +
+            "FOREIGN KEY(TipoComidaID) REFERENCES TipoComida(id))";
     final String SQL_CREATE_InfoUsuario = "CREATE TABLE InfoUsuario (id INTEGER PRIMARY KEY AUTOINCREMENT, UsuarioID INTEGER," +
             " TarjetaID INTEGER, Nombre TEXT, Telefono TEXT, Correo TEXT, PlacaVehiculo TEXT," +
-            " FOREIGN KEY(UsuarioID) REFERENCES Usuario(id)), FOREIGN KEY(TarjetaID) REFERENCES Tarjeta(id)))";
-     */
+            " FOREIGN KEY(UsuarioID) REFERENCES Usuario(id), FOREIGN KEY(TarjetaID) REFERENCES Tarjeta(id))";
+    final String SQL_CREATE_ResxGer = "CREATE TABLE RestauranteXGerente (id INTEGER PRIMARY KEY AUTOINCREMENT, RestauranteID INTEGER," +
+            " InfoUsuarioID INTEGER," +
+            " FOREIGN KEY(RestauranteID) REFERENCES Restaurante(id), FOREIGN KEY(InfoUsuarioID) REFERENCES InfoUsuario(id))";
 
 
 
@@ -32,12 +37,15 @@ public class ConexionSQLite extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(SQL_CREATE_TipoAcceso);
         sqLiteDatabase.execSQL(SQL_CREATE_Usuario);
-        /*
         sqLiteDatabase.execSQL(SQL_CREATE_TipoComida);
         sqLiteDatabase.execSQL(SQL_CREATE_Tarjeta);
-        sqLiteDatabase.execSQL(SQL_CREATE_Plato);
         sqLiteDatabase.execSQL(SQL_CREATE_InfoUsuario);
-        */
+        sqLiteDatabase.execSQL(SQL_CREATE_Plato);
+        sqLiteDatabase.execSQL(SQL_CREATE_Direccion);
+        sqLiteDatabase.execSQL(SQL_CREATE_Restaurante);
+        sqLiteDatabase.execSQL(SQL_CREATE_ResxGer);
+
+
 
     }
 
