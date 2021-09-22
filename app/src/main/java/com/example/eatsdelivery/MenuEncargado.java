6e495c16502065ff12f3ede3162d6436dcc54fa7
Marco Reveiz <52094428+revv-tech@users.noreply.github.com>
2021-09-21 18:04:12 -0600

    Prueba

--- app/src/main/java/com/example/eatsdelivery/MenuEncargado.java
+++ app/src/main/java/com/example/eatsdelivery/MenuEncargado.java
@@ -8,29 +8,38 @@ import android.view.View;
 
 public class MenuEncargado extends AppCompatActivity {
 
+    Object idEncargado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_encargado);
+
+        Object idEncargado = getIntent().getStringExtra("userID");
     }
     public void editMenuRest(View view){
         Intent next = new Intent(this, EditMenuEncargado.class);
+        next.putExtra("idEncargado", idEncargado.toString());
         startActivity(next);
     }
     public void updateInfoRest(View view){
         Intent next = new Intent(this, EditInfoRestEncargado.class);
+        next.putExtra("idEncargado", idEncargado.toString());
         startActivity(next);
     }
     public void manageOrders(View view){
         Intent next = new Intent(this, OrderManageEncargado.class);
+        next.putExtra("idEncargado", idEncargado.toString());
         startActivity(next);
     }
     public void requestDeletetion(View view){
         Intent next = new Intent(this, DeleteRequestEncargado.class);
+        next.putExtra("idEncargado", idEncargado.toString());
         startActivity(next);
     }
     public void quit(View view){
         Intent next = new Intent(this, MainActivity.class);
+        next.putExtra("idEncargado", idEncargado.toString());
         startActivity(next);
     }
 }
