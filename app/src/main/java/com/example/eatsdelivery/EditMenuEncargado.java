1725bf64d27af5d06cb605dd3ded6e981b0d0ceb
Sebastian <thelopezepol@estudiantec.cr>
2021-09-23 01:08:35 -0600

    prueba1

--- app/src/main/java/com/example/eatsdelivery/EditMenuEncargado.java
+++ app/src/main/java/com/example/eatsdelivery/EditMenuEncargado.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 
 public class EditMenuEncargado extends AppCompatActivity {
+    Object idEncargado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_menu_encargado);
+
+        
     }
 
     public void addProductWindow(View view){
